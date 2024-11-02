348d0a54fe7077f2f5ce44026a848bd4a1fa05d9
KatieWoe <kawo7071@colorado.edu>
2024-11-02 16:50:35 +0000

    dbselect

--- app.py
+++ app.py
@@ -44,3 +44,23 @@ def inserting():
     conn.commit()
     conn.close()
     return "Basketball Table Successfully Populated"
+
+@app.route('db_select')
+def selecting():
+    conn = psycopg2.connect("postgresql://katie_3308_lab10_user:RV4E0w9kat01p8aZxkf80Yx5hbHGLzkX@dpg-csj511q3esus7380aaf0-a/katie_3308_lab10")
+    cur = conn.cursor()
+    cur.execute('''
+    SELECT * FROM Basketball;
+    ''')
+    records = cur.fetchall()
+    conn.commit()
+    conn.close()
+    response_string = ""
+    response_string += "<table>
+    for player in records:
+        response_string += "<tr>"
+        for info in player:
+            response_string += "<td>{}</td>".format(info)
+        response_string += "</tr>"
+    response_string += "</table>"
+    return response_string
